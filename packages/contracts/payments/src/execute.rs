a345546296241225f441f25ffa6a9ebecb16d6b7
Ethan Frey <ethanfrey@noreply.github.com>
2025-10-27 19:00:31 +0100

    Use raw MsgSend to send from a different account

--- packages/contracts/payments/src/execute.rs
+++ packages/contracts/payments/src/execute.rs
@@ -1,7 +1,8 @@
 use crate::state::{
     ADMIN, ALLOWED_DENOMS, FUNDED_ACCOUNTS, OPEN_ACCOUNTS, PENDING_PAYMENTS, SERVICE_MANAGER,
 };
-use cosmwasm_std::{ensure, BankMsg, Coin, DepsMut, Env, MessageInfo, Response, Uint256};
+use cosmwasm_std::{ensure, AnyMsg, BankMsg, Coin, DepsMut, Env, MessageInfo, Response, Uint256};
+use layer_climb_proto::{bank::MsgSend, Coin as ProtoCoin, Message, Name};
 use tg_contract_api::payments::msg::{RegisterReceiveMsg, SendPaymentMsg, WavsPayload};
 use wavs_types::contracts::cosmwasm::service_manager::ServiceManagerQueryMessages;
 use wavs_types::contracts::cosmwasm::{
@@ -160,12 +161,19 @@ pub fn _send_payment(
         }
     };
 
-    // either send directly to receiver, or we send to self and keep
-    let msg = BankMsg::Send {
-        // TODO: how do we send from another account? StargateMsg???
-        // from_address: from_addr,
+    // We need to build a custom Any (protobuf) message to expose the power of sending from a different address
+    let send_val = ProtoCoin {
+        amount: amount.amount.to_string(),
+        denom: amount.denom.clone(),
+    };
+    let proto = MsgSend {
+        from_address: from_addr.to_string(),
         to_address: to_addr.to_string(),
-        amount: vec![amount.clone()],
+        amount: vec![send_val],
+    };
+    let msg = AnyMsg {
+        type_url: MsgSend::type_url(),
+        value: proto.encode_to_vec().into(),
     };
 
     Ok(Response::new()
